c6dc93318176d3b43aa9920f498b736972a140cd
Mikael <mikael.heininen@student.uwasa.fi>
2020-07-23 18:47:06 +0300

    react yt api, filter queue, show queue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import GetVideoService from './services/GetMusic'
+import YouTube from 'react-youtube';
 const App = () => {
   const [isPlayed, setIsPlayed] = useState('pFzL_8c494M')
-  const [youtubeUrl, setyoutubeUrl] = useState('https://www.youtube.com/embed/')
   const [autoplay, setAutoplay] = useState('?autoplay=1&mute=1&enablejsapi=1')
   const [userInput, setUserInput] = useState('')
   const [queryResults, setQueryResults] = useState<string[]>([])
-  const [songQueue, setSongQueue] = useState<string[]>([])
+  const [songQueue, setSongQueue] = useState<any>([])
   useEffect(() => {
     setQueryResults(queryResults)
   },[queryResults])
@@ -18,27 +18,43 @@ const App = () => {
   const change = (event:any) => {
     setUserInput(event.target.value)
   }
-  const putToQueue = (id:any): any => {
-    setSongQueue(songQueue.concat(id))
+  const putToQueue = (queryObject:any): any => {
+    setSongQueue(songQueue.concat(queryObject))
     setUserInput('')
     setQueryResults([])
   }
+  const takeNextSong = () => {
+    setSongQueue(songQueue.filter((obj:any) => obj.videoid !== isPlayed))
+    setIsPlayed(songQueue[0].videoid)
+  }
+  const showQueue = () =>
+  songQueue.map((song:any) => {
+    return(
+      <div>
+        <h5>{song.title}</h5>
+      </div>
+    )
+  })
   const getQueryResults = () => 
     queryResults.map((query:any) => {
     return(
       <div>
-        <h5>{query.title}</h5><button onClick={putToQueue(query.videoid)}>JONOON</button>
+        <h5>{query.title}</h5><button onClick={putToQueue(query)}>JONOON</button>
       </div>
     )
     }
   )
   return(
     <div>
-        <iframe width="420" height="345" title="soittaja" src={youtubeUrl+isPlayed+autoplay}>
-        </iframe> <br />
+        <YouTube
+          onEnd={takeNextSong}
+          videoId={isPlayed+autoplay}
+        />
         <input type = "text" value={userInput} name="userInput" onChange={change}></input> <button onClick = {makeQuery}>Etsi</button><br />
         Ehdotukset biisillesi: <br />
-        {getQueryResults()}
+        {getQueryResults()} <br /> <br />
+        Biisijono: <br />
+        {showQueue()}
     </div>
   )
 }
