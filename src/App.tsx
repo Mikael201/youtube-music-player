d3ff6ca65c792a9d98e6cd84429386f05bb9f459
Mikael <mikael.heininen@student.uwasa.fi>
2020-07-23 18:25:15 +0300

    queue, queries, iframe, input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
 import React, {useState, useEffect} from 'react';
 import GetVideoService from './services/GetMusic'
 const App = () => {
+  const [isPlayed, setIsPlayed] = useState('pFzL_8c494M')
+  const [youtubeUrl, setyoutubeUrl] = useState('https://www.youtube.com/embed/')
+  const [autoplay, setAutoplay] = useState('?autoplay=1&mute=1&enablejsapi=1')
+  const [userInput, setUserInput] = useState('')
+  const [queryResults, setQueryResults] = useState<string[]>([])
+  const [songQueue, setSongQueue] = useState<string[]>([])
   useEffect(() => {
-    GetVideoService.GetVideo("snöörit");
-  })
+    setQueryResults(queryResults)
+  },[queryResults])
+  const makeQuery = () => {
+    let titlesAndVideoIDs:string[] = GetVideoService.GetVideo(userInput);
+    setQueryResults(titlesAndVideoIDs)
+    console.log(queryResults)
+  }
+  const change = (event:any) => {
+    setUserInput(event.target.value)
+  }
+  const putToQueue = (id:any): any => {
+    setSongQueue(songQueue.concat(id))
+    setUserInput('')
+    setQueryResults([])
+  }
+  const getQueryResults = () => 
+    queryResults.map((query:any) => {
+    return(
+      <div>
+        <h5>{query.title}</h5><button onClick={putToQueue(query.videoid)}>JONOON</button>
+      </div>
+    )
+    }
+  )
   return(
     <div>
-      
+        <iframe width="420" height="345" title="soittaja" src={youtubeUrl+isPlayed+autoplay}>
+        </iframe> <br />
+        <input type = "text" value={userInput} name="userInput" onChange={change}></input> <button onClick = {makeQuery}>Etsi</button><br />
+        Ehdotukset biisillesi: <br />
+        {getQueryResults()}
     </div>
   )
 }
--- src/services/GetMusic.tsx
+++ src/services/GetMusic.tsx
@@ -2,18 +2,18 @@ import axios from "axios"
 const queryUrl = 'https://www.googleapis.com/youtube/v3/search?'
 const videoUrl = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&'
 //'https://www.googleapis.com/youtube/v3/search?q=pynnonen&key=AIzaSyCa0SSIFUFgQ3jdm4u2grfkOxFCLpeRP30'
-const GetVideo = (queryString: string | number) => {
-    console.log(queryUrl + `q=${queryString}&key=AIzaSyCa0SSIFUFgQ3jdm4u2grfkOxFCLpeRP30`)
-    axios.get(queryUrl + `q=${queryString}&key=AIzaSyCa0SSIFUFgQ3jdm4u2grfkOxFCLpeRP30`) //saadaan videoIdt
+const GetVideo = (queryString: string | number): string[] => {
+    let videoTitles: string[] = [];
+    let allDataArray: any = [];
+    axios.get(queryUrl + `q=${queryString}&key=AIzaSyCvyVY7J2AAfWJ1L2bFb9cZjyE57qVHTjU`) //saadaan videoIdt
         .then((allItems: any) => {
             let allTitles = allItems.data.items.map((itemsObject:any) => {
                 return itemsObject.id.videoId;
             })
-            let videoTitles: string[] = [];
-            allTitles.map((videoid:string|number) => {
+            let newAllTitles = allTitles.filter((f:any) => f) //remove undefines
+            newAllTitles.map((videoid:string|number) => {
                 if(videoid !== undefined) {
-                    console.log()
-                    axios.get(videoUrl + `id=${videoid}&key=AIzaSyCa0SSIFUFgQ3jdm4u2grfkOxFCLpeRP30`)
+                    axios.get(videoUrl + `id=${videoid}&key=AIzaSyCvyVY7J2AAfWJ1L2bFb9cZjyE57qVHTjU`)
                     .then(videodata => {
                         videodata.data.items.map((videoSpecificData: any) => {
                             videoTitles.push(videoSpecificData.snippet.title)
@@ -21,13 +21,17 @@ const GetVideo = (queryString: string | number) => {
                     }).catch(e => console.log(e))
                 }
             })
-            console.log(videoTitles) //tässä kaikki titlet
-            console.log(allItems) //tässä kaikki videoIdt  --> titlen ja allItems tarvii molemmat koska kun käyttis painaa niin kyseinen videoId täytyy lähteä päälle
-        })
-} //
+            for(let i = 0; i<newAllTitles.length; i++) {
+                allDataArray[i] = {
+                    videoid: videoTitles[i],
+                    title: newAllTitles[i]
+                }
+            }
+        }).catch(e => console.log(e))
+        console.log(allDataArray)
+        return allDataArray
+}
 
-//Käyttis kun kirjottaa niin eka haetaan sen queryllä eri videoIdt -> sen jälkeen haetaan videoIdeillä titlet
-//sitten ehdotetaan titlejä käyttikselle --> kun painaa titleen niin sen objectin videoId lisätään soitettavien biisien jonoon
 export default {
     GetVideo:GetVideo
 }
